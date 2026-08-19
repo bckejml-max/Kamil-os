7253b84803a3f05510a9ee67d3719ff18499034f
bckejml-max <bckejml@gmail.com>
2026-08-19 22:22:14 +0200

    Version Kamil OS 24 app shell

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='kamil-os-22.3.4-shell';
+const CACHE='kamil-os-24.0.0-shell';
 const ASSETS=['./','./index.html','./styles.css','./manifest.webmanifest','./js/config.js','./js/utils.js','./js/state.js','./js/cloud.js','./js/intelligence.js','./js/render.js','./js/command.js','./js/app.js','./js/preflight.js','./icons/icon-192.png','./icons/icon-512.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
