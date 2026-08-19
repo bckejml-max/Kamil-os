4a97cb97b13394b3d8db1c2b0a0aaed9cb159578
bckejml-max <bckejml@gmail.com>
2026-08-19 22:39:59 +0200

    Refresh Kamil OS 24.1 offline shell

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='kamil-os-24.0.0-shell-v3';
-const ASSETS=['./','./index.html','./styles.css','./styles24.css','./manifest.webmanifest','./js/config.js','./js/utils.js','./js/state.js','./js/cloud.js','./js/intelligence.js','./js/render.js','./js/today24.js','./js/work24.js','./js/money24.js','./js/tickets24.js','./js/command.js','./js/app.js','./js/preflight.js','./icons/icon-192.png','./icons/icon-512.png'];
+const CACHE='kamil-os-24.1.0-shell';
+const ASSETS=['./','./index.html','./styles.css','./styles24.css','./manifest.webmanifest','./js/config.js','./js/utils.js','./js/state.js','./js/cloud.js','./js/intelligence.js','./js/render.js','./js/today24.js','./js/work24.js','./js/money24.js','./js/tickets24.js','./js/more24.js','./js/command.js','./js/app.js','./js/preflight.js','./icons/icon-192.png','./icons/icon-512.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>{
