851f5bf96cb5b12a0f394ede5d96557d0fcb3a96
bckejml-max <bckejml@gmail.com>
2026-08-20 16:44:33 +0200

    release: bump Kamil OS to 29.5

--- js/config.js
+++ js/config.js
@@ -1,4 +1,4 @@
-export const APP_VERSION = '29.4.0';
+export const APP_VERSION = '29.5.0';
 export const SCHEMA_VERSION = 41;
 export const SUPABASE_URL = 'https://tswqfbkmxywxxczsoddr.supabase.co';
 export const SUPABASE_KEY = 'sb_publishable_kLy9FrQ7cpNEVhvqcCEsfw_MFVqBlFg';
