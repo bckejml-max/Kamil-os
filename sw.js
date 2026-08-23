8e0e6e9e7653cbc0c8b38846f2b49f1163ade36f
bckejml-max <bckejml@gmail.com>
2026-08-24 00:32:26 +0200

    65.6 precache Ticket Market Watch

--- sw.js
+++ sw.js
@@ -1,8 +1,9 @@
-const CACHE='kamil-os-65.5-morning-launch-r1';
+const CACHE='kamil-os-65.6-ticket-market-watch-r1';
 const SHELL=[
   './','./index.html','./manifest.webmanifest','./styles.css','./theme33.css','./personal64.css','./personal65.css',
+  './data/ticket_inventory_2026.json',
   './js/instantShell64.js','./js/app.js','./js/releaseMeta.js','./js/config.js','./js/state.js','./js/utils.js',
-  './js/viewRuntime41.js','./js/personalShell640.js','./js/personalHardening650.js','./js/personalVault640.js','./js/personalActions640.js','./js/personalAsk640.js','./js/personalAssistant650.js','./js/personalWaiting650.js','./js/personalUsage650.js','./js/personalDate650.js','./js/personalDailyRhythm651.js','./js/personalTomorrow653.js','./js/personalMorning655.js',
+  './js/viewRuntime41.js','./js/personalShell640.js','./js/personalHardening650.js','./js/personalVault640.js','./js/personalActions640.js','./js/personalAsk640.js','./js/personalAssistant650.js','./js/personalWaiting650.js','./js/personalUsage650.js','./js/personalDate650.js','./js/personalDailyRhythm651.js','./js/personalTomorrow653.js','./js/personalMorning655.js','./js/ticketMarketWatch656.js',
   './js/personalToday640.js','./js/personalFamily640.js','./js/personalHome640.js','./js/personalMoney640.js','./js/personalDocuments640.js','./js/personalMore640.js',
   './js/personalActionExecution641.js','./js/personalVaultEdit641.js','./js/personalFollowup642.js','./js/personalCapture643.js','./js/personalFamilyHomeActions644.js','./js/personalMoneyActions645.js','./js/personalDocumentActions646.js','./js/personalSettings647.js',
   './js/cloud.js','./js/cloudPayload32.js','./js/authUx32.js','./js/coldPartition42.js','./js/perf41.js',
