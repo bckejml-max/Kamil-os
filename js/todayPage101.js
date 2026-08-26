0e4f699413612309334985dcfc63aae2b45d366c
bckejml-max <bckejml@gmail.com>
2026-08-26 15:22:07 +0200

    OS 115: mount risk budget

--- js/todayPage101.js
+++ js/todayPage101.js
@@ -4,10 +4,11 @@ import {enhanceOS111} from './os111.js';
 import {enhanceOS112} from './os112.js';
 import {enhanceOS113} from './os113.js';
 import {enhanceOS114} from './os114.js';
+import {enhanceOS115} from './os115.js';
 import {renderPersonalToday640} from './personalToday640.js';
 
 function ensureStyle(){
- const styles=[['dashboard110','./dashboard110.css'],['dashboard1103','./dashboard1103.css'],['os111','./os111.css'],['os112','./os112.css'],['os113','./os113.css'],['os114','./os114.css']];
+ const styles=[['dashboard110','./dashboard110.css'],['dashboard1103','./dashboard1103.css'],['os111','./os111.css'],['os112','./os112.css'],['os113','./os113.css'],['os114','./os114.css'],['os115','./os115.css']];
  for(const [key,href] of styles){if(!document.querySelector(`link[data-${key}]`)){const l=document.createElement('link');l.rel='stylesheet';l.href=href;l.setAttribute(`data-${key}`,'1');document.head.appendChild(l)}}
 }
-export function renderTodayPage101(){ensureStyle();renderDashboard1103().then(()=>{enhanceXtbReview110();return enhanceOS111()}).then(()=>enhanceOS112()).then(()=>enhanceOS113()).then(()=>enhanceOS114()).catch(e=>{console.error('[dashboard114]',e);renderPersonalToday640()})}
+export function renderTodayPage101(){ensureStyle();renderDashboard1103().then(()=>{enhanceXtbReview110();return enhanceOS111()}).then(()=>enhanceOS112()).then(()=>enhanceOS113()).then(()=>enhanceOS114()).then(()=>enhanceOS115()).catch(e=>{console.error('[dashboard115]',e);renderPersonalToday640()})}
