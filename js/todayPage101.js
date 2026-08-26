67fb6b4b3f48cf6c9af7dfa87cf69a0b1783e8ad
bckejml-max <bckejml@gmail.com>
2026-08-26 14:33:19 +0200

    OS 110.3: load live FX styling

--- js/todayPage101.js
+++ js/todayPage101.js
@@ -2,5 +2,5 @@ import {renderDashboard1103} from './dashboardFx1103.js';
 import {enhanceXtbReview110} from './xtbReview110.js';
 import {renderPersonalToday640} from './personalToday640.js';
 
-function ensureStyle(){if(document.querySelector('link[data-dashboard110]'))return;const l=document.createElement('link');l.rel='stylesheet';l.href='./dashboard110.css';l.dataset.dashboard110='1';document.head.appendChild(l)}
+function ensureStyle(){if(!document.querySelector('link[data-dashboard110]')){const l=document.createElement('link');l.rel='stylesheet';l.href='./dashboard110.css';l.dataset.dashboard110='1';document.head.appendChild(l)}if(!document.querySelector('link[data-dashboard1103]')){const l=document.createElement('link');l.rel='stylesheet';l.href='./dashboard1103.css';l.dataset.dashboard1103='1';document.head.appendChild(l)}}
 export function renderTodayPage101(){ensureStyle();renderDashboard1103().then(()=>enhanceXtbReview110()).catch(e=>{console.error('[dashboard1103]',e);renderPersonalToday640()})}
