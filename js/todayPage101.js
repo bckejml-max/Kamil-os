cf4047bfa03811f2c5e55089b57b4434ae215f86
bckejml-max <bckejml@gmail.com>
2026-08-26 14:49:26 +0200

    OS 111: mount visual command center enhancements

--- js/todayPage101.js
+++ js/todayPage101.js
@@ -1,6 +1,7 @@
 import {renderDashboard1103} from './dashboardFx1103.js';
 import {enhanceXtbReview110} from './xtbReview110.js';
+import {enhanceOS111} from './os111.js';
 import {renderPersonalToday640} from './personalToday640.js';
 
-function ensureStyle(){if(!document.querySelector('link[data-dashboard110]')){const l=document.createElement('link');l.rel='stylesheet';l.href='./dashboard110.css';l.dataset.dashboard110='1';document.head.appendChild(l)}if(!document.querySelector('link[data-dashboard1103]')){const l=document.createElement('link');l.rel='stylesheet';l.href='./dashboard1103.css';l.dataset.dashboard1103='1';document.head.appendChild(l)}}
-export function renderTodayPage101(){ensureStyle();renderDashboard1103().then(()=>enhanceXtbReview110()).catch(e=>{console.error('[dashboard1103]',e);renderPersonalToday640()})}
+function ensureStyle(){if(!document.querySelector('link[data-dashboard110]')){const l=document.createElement('link');l.rel='stylesheet';l.href='./dashboard110.css';l.dataset.dashboard110='1';document.head.appendChild(l)}if(!document.querySelector('link[data-dashboard1103]')){const l=document.createElement('link');l.rel='stylesheet';l.href='./dashboard1103.css';l.dataset.dashboard1103='1';document.head.appendChild(l)}if(!document.querySelector('link[data-os111]')){const l=document.createElement('link');l.rel='stylesheet';l.href='./os111.css';l.dataset.os111='1';document.head.appendChild(l)}}
+export function renderTodayPage101(){ensureStyle();renderDashboard1103().then(()=>{enhanceXtbReview110();return enhanceOS111()}).catch(e=>{console.error('[dashboard111]',e);renderPersonalToday640()})}
