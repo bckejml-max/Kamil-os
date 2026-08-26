4d725305c1e9aa7d05f4566235745fe075f6de3c
bckejml-max <bckejml@gmail.com>
2026-08-26 14:32:41 +0200

    OS 110.3: mount live FX command center

--- js/todayPage101.js
+++ js/todayPage101.js
@@ -1,6 +1,6 @@
-import {renderDashboard110} from './dashboard110.js';
+import {renderDashboard1103} from './dashboardFx1103.js';
 import {enhanceXtbReview110} from './xtbReview110.js';
 import {renderPersonalToday640} from './personalToday640.js';
 
 function ensureStyle(){if(document.querySelector('link[data-dashboard110]'))return;const l=document.createElement('link');l.rel='stylesheet';l.href='./dashboard110.css';l.dataset.dashboard110='1';document.head.appendChild(l)}
-export function renderTodayPage101(){ensureStyle();renderDashboard110().then(()=>enhanceXtbReview110()).catch(e=>{console.error('[dashboard110]',e);renderPersonalToday640()})}
+export function renderTodayPage101(){ensureStyle();renderDashboard1103().then(()=>enhanceXtbReview110()).catch(e=>{console.error('[dashboard1103]',e);renderPersonalToday640()})}
