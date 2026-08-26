2550d2ab6fb1f3e1003c80fc1cbda603793cf744
bckejml-max <bckejml@gmail.com>
2026-08-26 18:53:00 +0200

    OS 118: mount scenario lab

--- js/todayPage101.js
+++ js/todayPage101.js
@@ -7,10 +7,11 @@ import {enhanceOS114} from './os114.js';
 import {enhanceOS115} from './os115.js';
 import {enhanceOS116} from './os116.js';
 import {enhanceOS117} from './os117.js';
+import {enhanceOS118} from './os118.js';
 import {renderPersonalToday640} from './personalToday640.js';
 
 function ensureStyle(){
- const styles=[['dashboard110','./dashboard110.css'],['dashboard1103','./dashboard1103.css'],['os111','./os111.css'],['os112','./os112.css'],['os113','./os113.css'],['os114','./os114.css'],['os115','./os115.css'],['os116','./os116.css'],['os117','./os117.css']];
+ const styles=[['dashboard110','./dashboard110.css'],['dashboard1103','./dashboard1103.css'],['os111','./os111.css'],['os112','./os112.css'],['os113','./os113.css'],['os114','./os114.css'],['os115','./os115.css'],['os116','./os116.css'],['os117','./os117.css'],['os118','./os118.css']];
  for(const [key,href] of styles){if(!document.querySelector(`link[data-${key}]`)){const l=document.createElement('link');l.rel='stylesheet';l.href=href;l.setAttribute(`data-${key}`,'1');document.head.appendChild(l)}}
 }
-export function renderTodayPage101(){ensureStyle();renderDashboard1103().then(()=>{enhanceXtbReview110();return enhanceOS111()}).then(()=>enhanceOS112()).then(()=>enhanceOS113()).then(()=>enhanceOS114()).then(()=>enhanceOS115()).then(()=>enhanceOS116()).then(()=>enhanceOS117()).catch(e=>{console.error('[dashboard117]',e);renderPersonalToday640()})}
+export function renderTodayPage101(){ensureStyle();renderDashboard1103().then(()=>{enhanceXtbReview110();return enhanceOS111()}).then(()=>enhanceOS112()).then(()=>enhanceOS113()).then(()=>enhanceOS114()).then(()=>enhanceOS115()).then(()=>enhanceOS116()).then(()=>enhanceOS117()).then(()=>enhanceOS118()).catch(e=>{console.error('[dashboard118]',e);renderPersonalToday640()})}
