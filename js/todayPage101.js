ae2be92f7bad91a0f5ea068c7777abf592d26ad6
bckejml-max <bckejml@gmail.com>
2026-08-26 15:24:54 +0200

    OS 116: mount investment opportunity radar

--- js/todayPage101.js
+++ js/todayPage101.js
@@ -5,10 +5,11 @@ import {enhanceOS112} from './os112.js';
 import {enhanceOS113} from './os113.js';
 import {enhanceOS114} from './os114.js';
 import {enhanceOS115} from './os115.js';
+import {enhanceOS116} from './os116.js';
 import {renderPersonalToday640} from './personalToday640.js';
 
 function ensureStyle(){
- const styles=[['dashboard110','./dashboard110.css'],['dashboard1103','./dashboard1103.css'],['os111','./os111.css'],['os112','./os112.css'],['os113','./os113.css'],['os114','./os114.css'],['os115','./os115.css']];
+ const styles=[['dashboard110','./dashboard110.css'],['dashboard1103','./dashboard1103.css'],['os111','./os111.css'],['os112','./os112.css'],['os113','./os113.css'],['os114','./os114.css'],['os115','./os115.css'],['os116','./os116.css']];
  for(const [key,href] of styles){if(!document.querySelector(`link[data-${key}]`)){const l=document.createElement('link');l.rel='stylesheet';l.href=href;l.setAttribute(`data-${key}`,'1');document.head.appendChild(l)}}
 }
-export function renderTodayPage101(){ensureStyle();renderDashboard1103().then(()=>{enhanceXtbReview110();return enhanceOS111()}).then(()=>enhanceOS112()).then(()=>enhanceOS113()).then(()=>enhanceOS114()).then(()=>enhanceOS115()).catch(e=>{console.error('[dashboard115]',e);renderPersonalToday640()})}
+export function renderTodayPage101(){ensureStyle();renderDashboard1103().then(()=>{enhanceXtbReview110();return enhanceOS111()}).then(()=>enhanceOS112()).then(()=>enhanceOS113()).then(()=>enhanceOS114()).then(()=>enhanceOS115()).then(()=>enhanceOS116()).catch(e=>{console.error('[dashboard116]',e);renderPersonalToday640()})}
