cea87884026fe57360317c5c877d71d643d10a6c
bckejml-max <bckejml@gmail.com>
2026-08-26 21:35:31 +0200

    OS 141: wrap Inbox renderer

---
+++ js/inboxPage141.js
@@ -0,0 +1,3 @@
+import {renderPersonalInbox690} from './personalInbox690.js';
+import {enhanceInboxVisual141} from './inboxDocumentsVisual141.js';
+export async function renderInboxPage141(){await renderPersonalInbox690();try{enhanceInboxVisual141()}catch(e){console.warn('[inbox141]',e)}}
