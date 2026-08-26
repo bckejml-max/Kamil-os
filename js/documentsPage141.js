ffce4622f15507f01eb9b78cc5172b3f7028b530
bckejml-max <bckejml@gmail.com>
2026-08-26 21:35:41 +0200

    OS 141: wrap Documents renderer

---
+++ js/documentsPage141.js
@@ -0,0 +1,3 @@
+import {renderPersonalDocuments640} from './personalDocuments640.js';
+import {enhanceDocumentsVisual141} from './inboxDocumentsVisual141.js';
+export function renderDocumentsPage141(){renderPersonalDocuments640();try{enhanceDocumentsVisual141()}catch(e){console.warn('[documents141]',e)}}
