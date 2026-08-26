51ed2e88d7b6b77968d732b14230e01754c92f7f
bckejml-max <bckejml@gmail.com>
2026-08-26 19:02:29 +0200

    OS 120: mount integrated intelligence suite

--- js/todayPage101.js
+++ js/todayPage101.js
@@ -9,10 +9,7 @@ import {enhanceOS116} from './os116.js';
 import {enhanceOS117} from './os117.js';
 import {enhanceOS118} from './os118.js';
 import {enhanceOS119} from './os119.js';
+import {enhanceOS120} from './os120.js';
 import {renderPersonalToday640} from './personalToday640.js';
-
-function ensureStyle(){
- const styles=[['dashboard110','./dashboard110.css'],['dashboard1103','./dashboard1103.css'],['os111','./os111.css'],['os112','./os112.css'],['os113','./os113.css'],['os114','./os114.css'],['os115','./os115.css'],['os116','./os116.css'],['os117','./os117.css'],['os118','./os118.css'],['os119','./os119.css']];
- for(const [key,href] of styles){if(!document.querySelector(`link[data-${key}]`)){const l=document.createElement('link');l.rel='stylesheet';l.href=href;l.setAttribute(`data-${key}`,'1');document.head.appendChild(l)}}
-}
-export function renderTodayPage101(){ensureStyle();renderDashboard1103().then(()=>{enhanceXtbReview110();return enhanceOS111()}).then(()=>enhanceOS112()).then(()=>enhanceOS113()).then(()=>enhanceOS114()).then(()=>enhanceOS115()).then(()=>enhanceOS116()).then(()=>enhanceOS117()).then(()=>enhanceOS118()).then(()=>enhanceOS119()).catch(e=>{console.error('[dashboard119]',e);renderPersonalToday640()})}
+function ensureStyle(){const styles=[['dashboard110','./dashboard110.css'],['dashboard1103','./dashboard1103.css'],['os111','./os111.css'],['os112','./os112.css'],['os113','./os113.css'],['os114','./os114.css'],['os115','./os115.css'],['os116','./os116.css'],['os117','./os117.css'],['os118','./os118.css'],['os119','./os119.css'],['os120','./os120.css']];for(const [key,href] of styles){if(!document.querySelector(`link[data-${key}]`)){const l=document.createElement('link');l.rel='stylesheet';l.href=href;l.setAttribute(`data-${key}`,'1');document.head.appendChild(l)}}}
+export function renderTodayPage101(){ensureStyle();renderDashboard1103().then(()=>{enhanceXtbReview110();return enhanceOS111()}).then(()=>enhanceOS112()).then(()=>enhanceOS113()).then(()=>enhanceOS114()).then(()=>enhanceOS115()).then(()=>enhanceOS116()).then(()=>enhanceOS117()).then(()=>enhanceOS118()).then(()=>enhanceOS119()).then(()=>enhanceOS120()).catch(e=>{console.error('[dashboard120]',e);renderPersonalToday640()})}
