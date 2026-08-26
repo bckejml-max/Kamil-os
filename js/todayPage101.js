d7a0a6ff0160a1954af83ed41af658bf77cc64d7
bckejml-max <bckejml@gmail.com>
2026-08-26 14:55:18 +0200

    OS 112: mount executive intelligence

--- js/todayPage101.js
+++ js/todayPage101.js
@@ -1,7 +1,8 @@
 import {renderDashboard1103} from './dashboardFx1103.js';
 import {enhanceXtbReview110} from './xtbReview110.js';
 import {enhanceOS111} from './os111.js';
+import {enhanceOS112} from './os112.js';
 import {renderPersonalToday640} from './personalToday640.js';
 
-function ensureStyle(){if(!document.querySelector('link[data-dashboard110]')){const l=document.createElement('link');l.rel='stylesheet';l.href='./dashboard110.css';l.dataset.dashboard110='1';document.head.appendChild(l)}if(!document.querySelector('link[data-dashboard1103]')){const l=document.createElement('link');l.rel='stylesheet';l.href='./dashboard1103.css';l.dataset.dashboard1103='1';document.head.appendChild(l)}if(!document.querySelector('link[data-os111]')){const l=document.createElement('link');l.rel='stylesheet';l.href='./os111.css';l.dataset.os111='1';document.head.appendChild(l)}}
-export function renderTodayPage101(){ensureStyle();renderDashboard1103().then(()=>{enhanceXtbReview110();return enhanceOS111()}).catch(e=>{console.error('[dashboard111]',e);renderPersonalToday640()})}
+function ensureStyle(){if(!document.querySelector('link[data-dashboard110]')){const l=document.createElement('link');l.rel='stylesheet';l.href='./dashboard110.css';l.dataset.dashboard110='1';document.head.appendChild(l)}if(!document.querySelector('link[data-dashboard1103]')){const l=document.createElement('link');l.rel='stylesheet';l.href='./dashboard1103.css';l.dataset.dashboard1103='1';document.head.appendChild(l)}if(!document.querySelector('link[data-os111]')){const l=document.createElement('link');l.rel='stylesheet';l.href='./os111.css';l.dataset.os111='1';document.head.appendChild(l)}if(!document.querySelector('link[data-os112]')){const l=document.createElement('link');l.rel='stylesheet';l.href='./os112.css';l.dataset.os112='1';document.head.appendChild(l)}}
+export function renderTodayPage101(){ensureStyle();renderDashboard1103().then(()=>{enhanceXtbReview110();return enhanceOS111()}).then(()=>enhanceOS112()).catch(e=>{console.error('[dashboard112]',e);renderPersonalToday640()})}
