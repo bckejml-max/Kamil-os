e8012ac9fa3efc24b96480b17c6d904b747cbd4c
bckejml-max <bckejml@gmail.com>
2026-08-28 20:48:30 +0200

    Bump Kamil OS to 334

--- js/releaseMeta.js
+++ js/releaseMeta.js
@@ -1,3 +1,3 @@
-export const APP_VERSION='333.0.0';
-export const APP_RELEASE='333.0';
-// unified command release 333
+export const APP_VERSION='334.0.0';
+export const APP_RELEASE='334.0';
+// focus radar release 334
