4e59a093e8122499bf9ca77637e2e6030e08c24f
bckejml-max <bckejml@gmail.com>
2026-08-28 23:17:20 +0200

    Stop OS226 from faking view changes

--- js/todayWake226.js
+++ js/todayWake226.js
@@ -8,14 +8,14 @@ function wakeToday226(reason='store'){
  clearTimeout(timer226);
  timer226=setTimeout(()=>{
   wakeCount226+=1;
-  window.dispatchEvent(new CustomEvent('kamil:view-change',{detail:'today'}));
-  window.__KAMIL_TODAY_WAKE226__={version:226,reason,wakeCount:wakeCount226,at:Date.now()};
+  window.dispatchEvent(new CustomEvent('kamil:today-wake',{detail:{reason,wakeCount:wakeCount226}}));
+  window.__KAMIL_TODAY_WAKE226__={version:226,reason,wakeCount:wakeCount226,at:Date.now(),passive:true};
  },20);
 }
 
 export function installTodayWake226(){
  if(unsubscribe226)return unsubscribe226;
  unsubscribe226=store.subscribe((_state,reason)=>wakeToday226(reason||'store'));
- window.__KAMIL_TODAY_WAKE226__={version:226,reason:'installed',wakeCount:wakeCount226,at:Date.now()};
+ window.__KAMIL_TODAY_WAKE226__={version:226,reason:'installed',wakeCount:wakeCount226,at:Date.now(),passive:true};
  return unsubscribe226;
 }
