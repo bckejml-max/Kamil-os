2e93f5fd910d44758f1263b45966264f31804301
bckejml-max <bckejml@gmail.com>
2026-08-28 22:49:23 +0200

    Release OS340 Ticket Portfolio

--- js/releaseMeta.js
+++ js/releaseMeta.js
@@ -1,3 +1,3 @@
-export const APP_VERSION='336.0.0';
-export const APP_RELEASE='336.0';
-// safe action execution release 336
+export const APP_VERSION='340.0.0';
+export const APP_RELEASE='340.0';
+// ticket portfolio inventory/sold release 340
