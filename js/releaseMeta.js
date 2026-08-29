898525b21e4c1f15e0d1fa8fd1afd3ec1bccd342
bckejml-max <bckejml@gmail.com>
2026-08-29 20:57:01 +0200

    OS366: release waiting-person tracking

--- js/releaseMeta.js
+++ js/releaseMeta.js
@@ -1,3 +1,3 @@
-export const APP_VERSION='365.0.0';
-export const APP_RELEASE='365.0';
-// Follow-up aware Today cockpit 365
+export const APP_VERSION='366.0.0';
+export const APP_RELEASE='366.0';
+// Waiting-person aware Today cockpit 366
