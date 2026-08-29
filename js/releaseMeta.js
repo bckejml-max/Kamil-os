04b038c22318b8cbc891befc9014a145fb7176b6
bckejml-max <bckejml@gmail.com>
2026-08-29 20:44:39 +0200

    OS364: bump release metadata

--- js/releaseMeta.js
+++ js/releaseMeta.js
@@ -1,3 +1,3 @@
-export const APP_VERSION='363.0.0';
-export const APP_RELEASE='363.0';
-// Unified Today decision cockpit 363
+export const APP_VERSION='364.0.0';
+export const APP_RELEASE='364.0';
+// Execution-state aware Today cockpit 364
