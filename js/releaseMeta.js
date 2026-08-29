1d025ae9bc48cf7df91b25ca6c57909130621b2c
bckejml-max <bckejml@gmail.com>
2026-08-29 21:33:58 +0200

    OS368: finalize XTB decision release

--- js/releaseMeta.js
+++ js/releaseMeta.js
@@ -1,3 +1,3 @@
 export const APP_VERSION='368.0.0';
 export const APP_RELEASE='368.0';
-// XTB Buy Hold Reduce Sell decision layer 368
+// XTB Buy Hold Reduce Sell decision engine + Today summary 368
