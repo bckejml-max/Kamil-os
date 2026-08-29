0886bea408e69f62505bd0d5fb4c5b4b12b207fe
bckejml-max <bckejml@gmail.com>
2026-08-29 21:44:04 +0200

    OS369: release ticket decision engine

--- js/releaseMeta.js
+++ js/releaseMeta.js
@@ -1,3 +1,3 @@
-export const APP_VERSION='368.0.0';
-export const APP_RELEASE='368.0';
-// XTB Buy Hold Reduce Sell decision engine + Today summary 368
+export const APP_VERSION='369.0.0';
+export const APP_RELEASE='369.0';
+// Ticket Buy Hold Sell Lower Skip decision engine + Today summary 369
