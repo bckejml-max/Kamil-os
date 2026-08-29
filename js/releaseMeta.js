88fc66ba5d5648a500ac182c22b87235507879b4
bckejml-max <bckejml@gmail.com>
2026-08-29 20:50:44 +0200

    OS365: release follow-up engine

--- js/releaseMeta.js
+++ js/releaseMeta.js
@@ -1,3 +1,3 @@
-export const APP_VERSION='364.0.0';
-export const APP_RELEASE='364.0';
-// Execution-state aware Today cockpit 364
+export const APP_VERSION='365.0.0';
+export const APP_RELEASE='365.0';
+// Follow-up aware Today cockpit 365
