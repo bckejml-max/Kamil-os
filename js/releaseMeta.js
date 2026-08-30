a1e6e44474a0b18131341eb0c1bdd00b92afcc44
bckejml-max <bckejml@gmail.com>
2026-08-30 17:57:03 +0200

    OS413: release ticket alert center

--- js/releaseMeta.js
+++ js/releaseMeta.js
@@ -1,3 +1,3 @@
-export const APP_VERSION='412.0.0';
-export const APP_RELEASE='412.0';
-// Tickets: proportional payout-email reconciliation, settlement data repair and cashflow forecast 412
+export const APP_VERSION='413.0.0';
+export const APP_RELEASE='413.0';
+// Tickets: price/action alerts, unread alert center, proportional payout reconciliation and cashflow 413
