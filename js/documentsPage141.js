ef7aceda2386d55a18504536cec51dfd96c7bae3
bckejml-max <bckejml@gmail.com>
2026-09-07 23:47:00 +0200

    consolidation: show document source freshness inline

--- js/documentsPage141.js
+++ js/documentsPage141.js
@@ -1,3 +1,4 @@
 import {renderPersonalDocuments640} from './personalDocuments640.js';
 import {enhanceDocumentsVisual141} from './inboxDocumentsVisual141.js';
-export function renderDocumentsPage141(){renderPersonalDocuments640();try{enhanceDocumentsVisual141()}catch(e){console.warn('[documents141]',e)}}
+import {mountSourceBadge} from './sourceRegistry.js';
+export function renderDocumentsPage141(){renderPersonalDocuments640();try{enhanceDocumentsVisual141()}catch(e){console.warn('[documents141]',e)}mountSourceBadge('DOCS','#moreView')}
