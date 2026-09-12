4fd7182602e0a79ef459ad489ff2e70eaf37024d
bckejml-max <bckejml@gmail.com>
2026-09-12 10:09:51 +0200

    Cache OS737 visual layer in the stable shell

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
-const CACHE='kamil-os-737.0.1-core-r4';
+const CACHE='kamil-os-737.0.1-core-r5';
 const CRITICAL=[
- './','./index.html','./manifest.webmanifest','./styles.css','./os2.css','./os2010.css',
+ './','./index.html','./manifest.webmanifest','./styles.css','./os2.css','./os2010.css','./os737.css',
  './js/instantShell64.js','./js/app.js','./js/releaseMeta.js','./js/config.js',
  './js/state.js','./js/utils.js','./js/viewRuntime41.js','./js/runtimeOwnership1100.js',
  './js/cloud.js','./js/authUx32.js','./js/perf41.js','./js/coldPartition42.js'
