c31113d94a0767965dc943c002470c6f2e8302e8
bckejml-max <bckejml@gmail.com>
2026-09-12 20:07:21 +0200

    Guard Follow Up lifecycle in OS1100

--- runtime_lifecycle_1108_guard.mjs
+++ runtime_lifecycle_1108_guard.mjs
@@ -27,6 +27,7 @@ const files=[
  ['./js/kamilBrain300.js','today.brain300'],
  ['./js/kamilBrain303.js','today.brain303'],
  ['./js/emailWorkflowUi35.js','email.workflow35'],
+ ['./js/followUpUi35.js','followup.ui35'],
  ['./js/directorUi34.js','director.ui34'],
  ['./js/systemDiagnostics421.js','system.diagnostics421'],
  ['./js/remoteInboxUi31.js','remote.inbox31'],
