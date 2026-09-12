1c92772c4686bca7efd856ef698b2ddd37afd070
bckejml-max <bckejml@gmail.com>
2026-09-12 20:11:26 +0200

    Guard Kamil Brain 301 lifecycle in OS1100

--- runtime_lifecycle_1108_guard.mjs
+++ runtime_lifecycle_1108_guard.mjs
@@ -27,6 +27,7 @@ const files=[
  ['./js/focusQueue335.js','today.focusQueue335'],
  ['./js/managerDeadlines481.js','manager.deadlines481'],
  ['./js/kamilBrain300.js','today.brain300'],
+ ['./js/kamilBrain301.js','today.brain301'],
  ['./js/kamilBrain303.js','today.brain303'],
  ['./js/emailWorkflowUi35.js','email.workflow35'],
  ['./js/followUpUi35.js','followup.ui35'],
