9d5705e8bad13b2c04f9222bdb7b595d4e674f37
bckejml-max <bckejml@gmail.com>
2026-09-12 20:13:30 +0200

    Guard Performance lifecycle in OS1100

--- runtime_lifecycle_1108_guard.mjs
+++ runtime_lifecycle_1108_guard.mjs
@@ -8,6 +8,7 @@ const files=[
  ['./js/bettingMissed566.js','betting.missed566'],
  ['./js/unifiedCommand333.js','core.unified333'],
  ['./js/domainOS328.js','core.domain328'],
+ ['./js/performance330.js','core.performance330'],
  ['./js/audit525.js','core.audit527'],
  ['./js/marketEdgeUi32.js','market.edge32'],
  ['./js/profitControlUi32.js','profit.control32'],
