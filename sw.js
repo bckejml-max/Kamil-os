d6e7c5e58f7012e62317701041edb92c3e5de601
bckejml-max <bckejml@gmail.com>
2026-09-23 11:12:56 +0200

    OS1325 bump release and rotate density cache

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='kamil-os-737.0.5-core-r13';
+const CACHE='kamil-os-737.0.6-core-r14';
 const CRITICAL=[
  './','./index.html','./manifest.webmanifest','./styles.css','./os2.css','./productReset1300.css',
  './js/osHardening1110.js','./js/dataIntegrity1130.js','./js/instantShell64.js','./js/app.js','./js/releaseMeta.js','./js/config.js',
