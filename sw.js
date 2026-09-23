1d7c0b181474a8051ac2feac57d87384fb82d4bb
bckejml-max <bckejml@gmail.com>
2026-09-23 23:36:12 +0200

    OS1334: rotate cache and precache decision surfaces

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
-const CACHE='kamil-os-737.0.14-core-r22';
+const CACHE='kamil-os-737.0.15-core-r23';
 const CRITICAL=[
- './','./index.html','./manifest.webmanifest','./styles.css','./os2.css','./productReset1300.css','./os1331.css','./os1332.css','./os1333.css',
+ './','./index.html','./manifest.webmanifest','./styles.css','./os2.css','./productReset1300.css','./os1331.css','./os1332.css','./os1333.css','./os1334.css',
  './js/osHardening1110.js','./js/dataIntegrity1130.js','./js/instantShell64.js','./js/app.js','./js/releaseMeta.js','./js/config.js',
  './js/state.js','./js/utils.js','./js/viewRuntime41.js','./js/runtimeOwnership1100.js','./js/todayPage2000.js','./js/workPage1300.js','./js/propertyPage1300.js','./js/tasksOverview.js','./js/moneyOverview.js','./js/ticketOverview.js','./js/bettingOverview.js',
  './js/cloud.js','./js/authUx32.js','./js/perf41.js','./js/coldPartition42.js'
