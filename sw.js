d0546605b96cb250755640f46e7d0d86180a2bdf
bckejml-max <bckejml@gmail.com>
2026-09-23 11:45:41 +0200

    OS1329 bump release and rotate Task cache

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='kamil-os-737.0.9-core-r17';
+const CACHE='kamil-os-737.0.10-core-r18';
 const CRITICAL=[
  './','./index.html','./manifest.webmanifest','./styles.css','./os2.css','./productReset1300.css',
  './js/osHardening1110.js','./js/dataIntegrity1130.js','./js/instantShell64.js','./js/app.js','./js/releaseMeta.js','./js/config.js',
