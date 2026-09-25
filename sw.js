13786267aa896e6d1d275b9586042663217eea4d
bckejml-max <bckejml@gmail.com>
2026-09-25 15:46:17 +0200

    Rotate cache for 737.0.43

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='kamil-os-737.0.42-core-r50';
+const CACHE='kamil-os-737.0.43-core-r51';
 const CRITICAL=[
  './','./index.html','./manifest.webmanifest','./styles.css','./os2.css','./productReset1300.css','./os1331.css','./os1332.css','./os1333.css','./os1334.css','./os1400.css','./os1500.css',
  './js/osHardening1110.js','./js/dataIntegrity1130.js','./js/instantShell64.js','./js/app.js','./js/releaseMeta.js','./js/config.js',
